02ac46c70a1b48aa332029ee3727d64c4cb72eb5
Naman Srivastava <110972759+un-identified@users.noreply.github.com>
2024-07-24 14:46:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://sl-health.onrender.com",
+		origin: "https://sl-health.vercel.app",
 		methods: [ "GET", "POST" ]
 	}
 })
