059358f027ceadb8659c064e429bd230ba5cbd1e
Naman Srivastava <110972759+un-identified@users.noreply.github.com>
2024-07-24 16:08:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -164,7 +164,7 @@ function App() {
 
     return (
         <>
-            <h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+            <h1 style={{ textAlign: "center"}}>VIDEO CALL INTERFACE</h1>
             <div className="container">
                 <div className="video-container">
                     <div className="video">
