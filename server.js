d00b0bc17240bf241c097e6ce84b6f123a1fed96
Naman Srivastava <110972759+un-identified@users.noreply.github.com>
2024-07-24 15:52:01 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Import cors
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Configure CORS
+app.use(cors({
+    origin: 'https://sl-health.vercel.app', // Your frontend domain
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+
+let users = {};
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
+    });
+
+    socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+        users[userToCall] = socket.id;
+        io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+    });
+
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
+    });
+
+    socket.on('handMovement', ({ to, prediction }) => {
+        io.to(to).emit('handMovement', prediction);
+    });
+});
+
+server.listen(5000, () => console.log('server is running on port 5000'));
